9c53f4033042c423ec3bf9b4f3fa5aa8c64c003c
Vítor Emanuel <vitoremanuel.dev@gmail.com>
2023-08-29 20:45:14 -0300

    Projeto concluído

--- lib/pages/to_do_list.dart
+++ lib/pages/to_do_list.dart
@@ -1,19 +1,137 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:to_do_list/models/tarefa.dart';
+import 'package:to_do_list/repositories/tarefa_repository.dart';
 
-class ToDoList extends StatefulWidget {
-  const ToDoList({super.key});
+class ToDoListPage extends StatefulWidget {
+  const ToDoListPage({super.key});
 
   @override
-  State<ToDoList> createState() => _ToDoListState();
+  State<ToDoListPage> createState() => _ToDoListPageState();
 }
 
-class _ToDoListState extends State<ToDoList> {
+class _ToDoListPageState extends State<ToDoListPage> {
+  var descricaoController = TextEditingController();
+  var _tarefas = const <Tarefa>[];
+  var tarefaRepository = TarefaRepository();
+  bool apenasNaoConcluidos = false;
+
+  @override
+  void initState() {
+    super.initState();
+    obterTarefas();
+  }
+
+  void obterTarefas() async {
+    if (apenasNaoConcluidos) {
+      _tarefas = await tarefaRepository.listarNaoConcluidas();
+    } else {
+      _tarefas = await tarefaRepository.listar();
+    }
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
-          appBar: AppBar(title: const Text("To do List", textAlign: TextAlign.center,),),
-      body: Container(),
+      backgroundColor: const Color.fromARGB(255, 216, 216, 216),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          descricaoController.text = "";
+          showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                title: const Text("Adicionar tarefa"),
+                content: TextField(
+                  controller: descricaoController,
+                ),
+                actions: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: const Text("Cancelar"),
+                  ),
+                  TextButton(
+                    onPressed: () async {
+                      await tarefaRepository
+                          .adicionar(Tarefa(descricaoController.text, false));
+                      Navigator.pop(context);
+                      setState(() {});
+                    },
+                    child: const Text("Adicionar"),
+                  )
+                ],
+              );
+            },
+          );
+        },
+        child: const Icon(Icons.add),
+      ),
+      appBar: AppBar(
+        title: SizedBox(
+          width: double.infinity,
+          child: Text(
+            "To do List",
+            textAlign: TextAlign.center,
+            style: GoogleFonts.lato(fontSize: 20),
+          ),
+        ),
+      ),
+      body: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        child: Column(
+          children: [
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    "Filtrar apenas não concluídas",
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  Switch(
+                    value: apenasNaoConcluidos,
+                    onChanged: (value) {
+                      apenasNaoConcluidos = value;
+                      obterTarefas();
+                    },
+                  )
+                ],
+              ),
+            ),
+            const Divider(),
+            Expanded(
+              child: ListView.builder(
+                itemCount: _tarefas.length,
+                itemBuilder: (context, index) {
+                  var tarefa = _tarefas[index];
+                  return Dismissible(
+                    onDismissed: (direction) async {
+                      await tarefaRepository.remover(tarefa.id);
+                      obterTarefas();
+                    },
+                    key: Key(tarefa.id),
+                    child: ListTile(
+                      title: Text(tarefa.descricao),
+                      trailing: Switch(
+                        value: tarefa.concluido,
+                        onChanged: (value) async {
+                          await tarefaRepository.alterar(tarefa.id, value);
+                          obterTarefas();
+                        },
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
     ));
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:to_do_list/main.dart';
 import 'package:to_do_list/my_app.dart';
 
 void main() {
